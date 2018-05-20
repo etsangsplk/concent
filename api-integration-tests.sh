387eb7c783d232779c60fd03b2694b12b66df61c
Radoslaw Wrzesien <radoslaw.wrzesien@codepoets.it>
2018-05-21 00:28:37 +0200

    Add cluster test for additional verification use case

--- api-integration-tests.sh
+++ api-integration-tests.sh
@@ -5,3 +5,4 @@ python3 concent_api/api-integration-force-accept-or-reject-results-test.py $1 &&
 python3 concent_api/api-integration-force-get-task-result-test.py $1 &&
 python3 concent_api/api-integration-force-payment.py $1 &&
 python3 concent_api/api-integration-force-report-computed-task-test.py $1
+python3 concent_api/api-integration-additional-verification-test.py $1
